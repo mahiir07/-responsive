44441cbef606b7ee02ad9981a8a2f310a46092ab
mahiir07 <imsehgal7@gmail.com>
2025-11-16 11:25:58 +0530

    done

---
+++ script.js
@@ -0,0 +1,126 @@
+var arr = [
+                         //images gallery-1//
+    
+    {name: "Petals of roses", image: "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=3786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "Animals of town", image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "the crowd of city", image: "https://images.unsplash.com/photo-1517732306149-e8f829eb588a?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "fruits of planet", image: "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    {name: "orange peeled", image: "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "web design", image: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+                       
+                         //images gallery-2//
+    {name: "light house", image: "https://images.unsplash.com/photo-1761901175711-b6e3dd720a66?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+    {name: "Nature", image: "https://plus.unsplash.com/premium_photo-1754767853559-1ad8f192f41e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwM3xDRHd1d1hKQWJFd3x8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+    {name: "grl prtrait", image: "https://images.unsplash.com/photo-1757997853073-d088630c87e7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMDh8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600" },
+    {name: "Buildings", image: "https://images.unsplash.com/photo-1744794073862-704a8afd2941?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMzF8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600" },
+    {name: "Street photography", image: "https://images.unsplash.com/photo-1759800805614-4c08b9bf9ba9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+    {name: "Travel,Photos", image: "https://images.unsplash.com/photo-1758557683300-45ae99df1342?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+    {name: "Nature View", image: "https://images.unsplash.com/photo-1758299892041-28192a311d2a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDUwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+
+                           //images gallery-3//
+    {name: "white flowers", image: "https://images.unsplash.com/photo-1741682044306-68905693a8e8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+    {name: "Rider",  image:"https://images.unsplash.com/photo-1760272541617-e86736799606?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ3fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Travel", image:"https://plus.unsplash.com/premium_photo-1698405316329-fd9c43d7e11c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Parrot" , image: "https://images.unsplash.com/photo-1506220926022-cc5c12acdb35?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDY1fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "wallpaper", image: "https://images.unsplash.com/photo-1756990683708-afd38ed20f1f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDk0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"}, 
+     {name: "3D render", image: "https://images.unsplash.com/photo-1757952400950-f8c04fd7a104?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "wallpapers", image: "https://images.unsplash.com/photo-1517519014922-8fc06b814a0e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE3N3xibzhqUUtUYUUwWXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+                              
+                           //images gallery-4//
+     {name: "Nature", image: "https://images.unsplash.com/photo-1761753481927-f6b701ac2c07?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDc2fDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Textures", image: "https://images.unsplash.com/photo-1758220829551-6d02d06798c7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8aVVJc25WdGpCMFl8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Artitecture", image: "https://images.unsplash.com/photo-1759833307569-302d70652fd5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fE04alZiTGJUUndzfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "People", image: "https://images.unsplash.com/photo-1738253145897-e133d52b8211?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Pink Flowers", image: "https://images.unsplash.com/photo-1761469792393-45304668f54a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDg1fDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Moon, Nature", image: "https://images.unsplash.com/photo-1743704990821-c1bed2e2e16d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0Mnw2c01WalRMU2tlUXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "3D render", image: "https://images.unsplash.com/photo-1761669973922-3e4107cf1136?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDYzfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},     
+
+
+                          //images gallery-5//
+     {name: "featured", image: "https://plus.unsplash.com/premium_photo-1761953111449-baa0a316ab45?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "3D render", image: "https://images.unsplash.com/photo-1754634266934-e24523beb2ca?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU5fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "3D render", image: "https://plus.unsplash.com/premium_photo-1757581531991-da0f33189b5c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDh8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},     
+     {name: "Flowers", image: "https://images.unsplash.com/photo-1755366204962-3cb4bc8452b1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE3NXxibzhqUUtUYUUwWXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "3D render", image: "https://plus.unsplash.com/premium_photo-1754767853559-1ad8f192f41e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwM3xDRHd1d1hKQWJFd3x8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "Film", image: "https://images.unsplash.com/photo-1761405378313-622deb755731?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfGhtZW52UWhVbXhNfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Wallpaper", image: "https://images.unsplash.com/photo-1760159201329-521cd85a5bf9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+
+                        //images gallery-6//
+     {name:"boat" , image: "https://plus.unsplash.com/premium_photo-1756175546724-9b0b97799105?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Waterfall", image: "https://images.unsplash.com/photo-1762029198761-aeff595ad71f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Moon with mountain", image: "https://images.unsplash.com/photo-1760729814064-0389b03c909d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDl8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Fox animal", image: "https://images.unsplash.com/photo-1757137910091-1cf071030691?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExMnxibzhqUUtUYUUwWXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "nature", image: "https://images.unsplash.com/photo-1755398104195-294a494782e8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687"},
+     {name: "Sunflower", image: "https://images.unsplash.com/photo-1755366204962-3cb4bc8452b1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE3NXxibzhqUUtUYUUwWXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "Animal", image: "https://images.unsplash.com/photo-1752170337854-09086f036696?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzMXxibzhqUUtUYUUwWXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "Flowers", image: " https://images.unsplash.com/photo-1747227825543-5da2c9f03222?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMyMnxibzhqUUtUYUUwWXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+
+                          //images gallery-7//
+     {name: "3D render", image: "https://plus.unsplash.com/premium_photo-1761211108987-c37052604d95?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExOHxDRHd1d1hKQWJFd3x8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "Waterfall", image: "https://images.unsplash.com/photo-1749103139830-db45c38de8af?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE1MXw2c01WalRMU2tlUXx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "Street photography", image: "https://images.unsplash.com/photo-1755095901053-8accdcea2f2b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Experimental", image: "https://images.unsplash.com/photo-1761746555964-54d189e38bfd?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfHFQWXNEenZKT1ljfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "ocean with mountains", image: "https://images.unsplash.com/photo-1756312091880-aa605a682833?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8RnpvM3p1T0hONnd8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "girl-portrait", image:"https://images.unsplash.com/photo-1761960084284-c3fa68e5c2cd?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Beautiful Tree with flowers", image: "https://images.unsplash.com/photo-1760883956955-31d8adb4e6d9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMTF8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+
+                         //images gallery-8//
+     {name: "Butterfly", image: "https://plus.unsplash.com/premium_photo-1761430187310-3a53c9eb70cc?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMjJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Artitecture", image: "https://images.unsplash.com/photo-1761562964782-4c971df72d29?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNzJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Leafs", image: "https://images.unsplash.com/photo-1761391963902-f3cc4850ecf4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0OTB8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "Lion", image: "https://images.unsplash.com/photo-1761216593132-0e19e14641d3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1NDN8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600" },
+     {name: "Home", image: "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"},
+     {name: "Artitech", image: "https://images.unsplash.com/photo-1762656668994-57d8933d59dd?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=600"},
+     {name: "3d Render", image: "https://images.unsplash.com/photo-1730451306804-f7d3b0a3c4d5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+    
+         //image gallery-9//
+         {name: "nature", image: "https://plus.unsplash.com/premium_photo-1761916092337-2125999f903c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+         {name: "Cute Monkey", image: "https://images.unsplash.com/photo-1762545611539-df4e46e7747e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=600"},
+    ]
+
+function showTheCards(){
+    var clutter = "";
+    arr.forEach(function(obj){
+        clutter += `<div class="box">
+        <img class="cursor-pointer" src="${obj.image}" alt="image">
+        <div class="caption">images </div>
+    </div>`;
+    })
+
+    document.querySelector(".container")
+    .innerHTML = clutter;
+}
+
+function handleSearchFunctionality(){
+    var input = document.querySelector("#searchinput");
+    
+
+    input
+    .addEventListener("focus", function(){
+        document.querySelector(".overlay").style.display = "block";
+    })
+
+    input
+    .addEventListener("blur", function(){
+        document.querySelector(".overlay").style.display = "none";
+    })
+
+    input
+    .addEventListener("input", function(){
+        const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+        var clutter = "";
+        filteredArray.forEach(function(obj){
+            clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`
+        })
+    document.querySelector(".searchdata").style.display = "block";
+        document.querySelector(".searchdata").innerHTML = clutter;
+    })
+}
+
+handleSearchFunctionality();
+showTheCards();
+
